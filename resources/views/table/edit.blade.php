abc21cb60dd1089fd6dc2fcf145503f8559ace00
richard <ri.oc.torres@outlook.com>
2020-04-26 21:44:06 -0400

    table edit view added

---
+++ resources/views/table/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <form action="{{route('tables.update', $t->id)}}" method="post">
+        @csrf
+        @method('put')
+        
+        <div>
+            <label for="number">number:</label>
+            <input type="number" name="number" id="number" value="{{$t->amount_people}}" class="@error('number') is-invalid @enderror">
+            @error('number')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+        
+        <div>
+            <label for="amount_people">Amount People:</label>
+            <input type="number" name="amount_people" id="amount_people" value="{{$t->amount_people}}" class="@error('amount_people') is-invalid @enderror">
+            @error('amount_people')
+                <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
+    
+        <button type="submit">Edit</button>
+    </form>
+
+@endsection
