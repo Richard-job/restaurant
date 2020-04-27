64bc9ea5a5fcfdc7f7c76fbd924dd0bff2eab221
richard <ri.oc.torres@outlook.com>
2020-04-26 21:11:56 -0400

    table seeder added

--- database/seeds/TableSeeder.php
+++ database/seeds/TableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Table;
 
 class TableSeeder extends Seeder
 {
@@ -11,6 +12,45 @@ class TableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Table::create([
+            'number' => 1,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 2,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 3,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 4,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 5,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 6,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 7,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 8,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 9,
+            'amount_people' => rand(1,6),
+        ])->save();
+        Table::create([
+            'number' => 10,
+            'amount_people' => rand(1,6),
+        ])->save();
     }
 }
