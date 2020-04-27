7e0d6273df089727fd740723cd295832184779a1
richard <ri.oc.torres@outlook.com>
2020-04-26 21:34:11 -0400

    table index view added

---
+++ resources/views/table/index.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <table>
+        <tr>
+            <th>Number:</th>
+            <th>Amount People</th>
+            <th>Is Used</th>
+        </tr>
+        @foreach ($tables as $t)
+            <tr>
+                <td>{{$t->number}}</td>
+                <td>{{$t->amount_people}}</td>
+                <td>{{$t->is_used}}</td>
+                <td><a href="{{route('tables.show',$t->id)}}">View</a></td>
+            </tr>
+        @endforeach
+    </table>
+
+@endsection
